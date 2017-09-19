ff8334f6de8615452d9ba446ee6ebe8cc4824e95
owen-d <ow.diehl@gmail.com>
2017-09-19 13:46:04 -0400

    iterative process map implementation

--- ex/1/1.10-interlude.scm
+++ ex/1/1.10-interlude.scm
@@ -6,6 +6,17 @@
       (cons (fn (car col))
             (map fn (cdr col)))))
 
+;; iterative process version. Hello, TCO!
+(define (iter-map fn col)
+  (define (_iter-map accum fn col)
+    (if (null? col)
+        accum
+        (_iter-map (append accum
+                           (list (fn (car col))))
+                   fn
+                   (cdr col))))
+  (_iter-map '() fn col))
+
 (define (fold fn init col)
   (if (null? col)
       init
